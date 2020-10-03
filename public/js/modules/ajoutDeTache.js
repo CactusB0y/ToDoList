e23bc43ac5bdd967653bf8c5e0798d8339661fa4
CactusB0y <68423704+CactusB0y@users.noreply.github.com>
2020-10-03 13:53:13 +0200

    btn ajouter fini

--- public/js/modules/ajoutDeTache.js
+++ public/js/modules/ajoutDeTache.js
@@ -4,26 +4,68 @@ export let btnAdd = document.getElementById('btnAddHere');
 export let divStockTask = document.getElementById('placeHere');
 export let divWithTask;
 export let spanWithTask;
+export let divCol10;
+export let divCol2;
+export let btnCol2;
 
 
 
 btnAdd.addEventListener('click', ()=>{
     divWithTask = document.createElement('div');
-    divStockTask.appendChild(divWithTask)
-    divWithTask.innerText = inputAdd.value
-    inputAdd.value = "";
+    divWithTask.setAttribute('class', 'row modifWithTask py-5 mt-5')
+    divStockTask.appendChild(divWithTask);
+    divCol10 = document.createElement('div');
+    divCol10.setAttribute('class', 'col-10 text-center');
+    divWithTask.appendChild(divCol10);
+    spanWithTask = document.createElement('span');
+    spanWithTask.setAttribute('class', 'spanWithTask');
+    spanWithTask.innerText = inputAdd.value;
+    divCol10.appendChild(spanWithTask);
+    divCol2 = document.createElement('div');
+    divCol2.setAttribute('class', 'col-2 text-center d-flex flex-column');
+    divWithTask.appendChild(divCol2);
+    for (let i = 0; i < 3; i++) {
+        btnCol2 = document.createElement('button');
+        if (i == 0) {
+            btnCol2.innerText = "validé";
+        } else if (i == 1){
+            btnCol2.innerText = "modifier";
+        } else {
+            btnCol2.innerText = "supprimer";
+        }
+        btnCol2.setAttribute('class', 'btnWithTask');
+        divCol2.appendChild(btnCol2);
+        inputAdd.value = "";
+    }   
 })
 
 leCorps.addEventListener('keydown', (e)=>{
     if(e.key == "Enter"){
         divWithTask = document.createElement('div');
-        divStockTask.appendChild(divWithTask)
-        divWithTask.innerText = inputAdd.value
+    divWithTask.setAttribute('class', 'row modifWithTask py-5 mt-5')
+    divStockTask.appendChild(divWithTask);
+    divCol10 = document.createElement('div');
+    divCol10.setAttribute('class', 'col-10 text-center');
+    divWithTask.appendChild(divCol10);
+    spanWithTask = document.createElement('span');
+    spanWithTask.setAttribute('class', 'spanWithTask');
+    spanWithTask.innerText = inputAdd.value;
+    divCol10.appendChild(spanWithTask);
+    divCol2 = document.createElement('div');
+    divCol2.setAttribute('class', 'col-2 text-center d-flex flex-column');
+    divWithTask.appendChild(divCol2);
+    for (let i = 0; i < 3; i++) {
+        btnCol2 = document.createElement('button');
+        if (i == 0) {
+            btnCol2.innerText = "validé";
+        } else if (i == 1){
+            btnCol2.innerText = "modifier";
+        } else {
+            btnCol2.innerText = "supprimer";
+        }
+        btnCol2.setAttribute('class', 'btnWithTask');
+        divCol2.appendChild(btnCol2);
         inputAdd.value = "";
     }
+    }
 })
-
-
-
-console.log(inputAdd);
-console.log(btnAdd);

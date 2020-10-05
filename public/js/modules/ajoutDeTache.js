464267682705a47ac0b7218455aa1aa7cb934c29
CactusB0y <68423704+CactusB0y@users.noreply.github.com>
2020-10-05 12:38:41 +0200

    finition

--- public/js/modules/ajoutDeTache.js
+++ public/js/modules/ajoutDeTache.js
@@ -11,38 +11,41 @@ export let btnCol2;
 
 //création d'une tache
 btnAdd.addEventListener('click', ()=>{
-    divWithTask = document.createElement('div');
-    divWithTask.setAttribute('class', 'row modifWithTask py-5 my-5');
-    divWithTask.setAttribute('style', 'background-color: white');
-    divWithTask.classList.add('encours');
-    divStockTask.appendChild(divWithTask);
-    divCol10 = document.createElement('div');
-    divCol10.setAttribute('class', 'col-10 text-center');
-    divWithTask.appendChild(divCol10);
-    spanWithTask = document.createElement('span');
-    spanWithTask.setAttribute('class', 'spanWithTask');
-    spanWithTask.innerText = inputAdd.value;
-    divCol10.appendChild(spanWithTask);
-    divCol2 = document.createElement('div');
-    divCol2.setAttribute('class', 'col-2 text-center d-flex flex-column');
-    divWithTask.appendChild(divCol2);
-    for (let i = 0; i < 3; i++) {
-        btnCol2 = document.createElement('button');
-        if (i == 0) {
-            btnCol2.innerText = "validé";
-        } else if (i == 1){
-            btnCol2.innerText = "modifier";
-        } else {
-            btnCol2.innerText = "supprimer";
-        }
-        btnCol2.setAttribute('class', 'btnWithTask');
-        divCol2.appendChild(btnCol2);
-        inputAdd.value = "";
-    }   
+    if (inputAdd.value != ""){
+        divWithTask = document.createElement('div');
+        divWithTask.setAttribute('class', 'row modifWithTask py-5 my-5');
+        divWithTask.setAttribute('style', 'background-color: white');
+        divWithTask.classList.add('encours');
+        divStockTask.appendChild(divWithTask);
+        divCol10 = document.createElement('div');
+        divCol10.setAttribute('class', 'col-10 text-center');
+        divWithTask.appendChild(divCol10);
+        spanWithTask = document.createElement('span');
+        spanWithTask.setAttribute('class', 'spanWithTask');
+        spanWithTask.innerText = inputAdd.value;
+        divCol10.appendChild(spanWithTask);
+        divCol2 = document.createElement('div');
+        divCol2.setAttribute('class', 'col-2 text-center d-flex flex-column');
+        divWithTask.appendChild(divCol2);
+        for (let i = 0; i < 3; i++) {
+            btnCol2 = document.createElement('button');
+            if (i == 0) {
+                btnCol2.innerText = "validé";
+            } else if (i == 1){
+                btnCol2.innerText = "modifier";
+                btnCol2.classList.add("jesus");
+            } else {
+                btnCol2.innerText = "supprimer";
+            }
+            btnCol2.setAttribute('class', 'btnWithTask');
+            divCol2.appendChild(btnCol2);
+            inputAdd.value = "";
+        }  
+    } 
 })
-
+//créationj d'un tache avec le bouton enter
 leCorps.addEventListener('keydown', (e)=>{
-    if(e.key == "Enter"){
+    if(e.key == "Enter" && inputAdd.value != ""){
         divWithTask = document.createElement('div');
         divWithTask.setAttribute('class', 'row modifWithTask py-5 my-5');
         divWithTask.setAttribute('style', 'background-color: white');
@@ -64,6 +67,7 @@ leCorps.addEventListener('keydown', (e)=>{
             btnCol2.innerText = "validé";
             } else if (i == 1){
             btnCol2.innerText = "modifier";
+            btnCol2.classList.add("jesus");
             } else {
             btnCol2.innerText = "supprimer";
             }
--- public/js/modules/validSuppModif.js
+++ public/js/modules/validSuppModif.js
@@ -48,3 +48,5 @@ leCorps.addEventListener('click', (e)=>{
 })
 
 
+
+
